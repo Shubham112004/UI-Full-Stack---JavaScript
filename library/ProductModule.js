108696de5f04ae99948d0b47f93bc3a1ef2b6cb7
shubham gaikwad <shubhamgaikwad2k4@gmail.com>
2024-06-07 13:18:34 +0530

    JS OOP

---
+++ library/ProductModule.js
@@ -0,0 +1,10 @@
+//module 
+
+const msg = "Product Details";
+export default function Tittle() {
+    return msg;
+}
+
+export function Details() {
+    return { Name: "Samsung TV", Price: 45825.56 };
+}
